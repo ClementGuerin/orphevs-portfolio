b5d2f4b599f5b64efbf17d5b1aed03e0b624f380
ClementGuerin <clement.guerin@hetic.net>
2018-12-12 03:21:32 +0100

    Add project page

--- pages/project/_id.vue
+++ pages/project/_id.vue
@@ -1,11 +1,65 @@
 <template>
-  <div>project</div>
+  <section class="project-page">
+    <div class="header" :style="headerBackground"></div>
+    <div class="content">
+      <div class="container">
+        <h1>{{ project.title }}</h1>
+        <h3>{{ project.category }}</h3>
+        <p>{{ project.description }}</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import Strapi from "strapi-sdk-javascript/build/main";
+const apiUrl = process.env.API_URL || "http://localhost:1337";
+const strapi = new Strapi(apiUrl);
 export default {
   data() {
-    return {};
+    return {
+      query: "",
+      project: this.$store.getters["projects/list"][0]
+    };
+  },
+  computed: {
+    id() {
+      return this.$route.params.id;
+    },
+    headerBackground() {
+      return "background-image: url(" + this.project.thumbnail.url + ");";
+    }
+  },
+  async fetch({ store, params }) {
+    const self = this;
+
+    store.commit("projects/emptyList");
+    const response = await strapi.request("post", "/graphql", {
+      data: {
+        query: `query {
+            projects(where: {
+              slug: "${params.id}"
+            }) {
+              _id
+              slug
+              title
+              category
+              description
+              thumbnail {
+                url
+              }
+            }
+          }
+          `
+      }
+    });
+    response.data.projects.forEach(project => {
+      project.thumbnail.url = `${apiUrl}${project.thumbnail.url}`;
+      store.commit("projects/add", {
+        id: project.id || project._id,
+        ...project
+      });
+    });
   }
 };
 </script>
