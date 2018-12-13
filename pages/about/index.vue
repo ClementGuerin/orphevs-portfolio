6e64fb5e86b2280941c775ee9be040aa7f854dc5
ClementGuerin <clement.guerin@hetic.net>
2018-12-13 03:14:48 +0100

    update about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -30,6 +30,19 @@
     <div class="contact">
       <div class="container">
         <h2 class="title">Contact me</h2>
+        <div class="form row justify-content-center">
+          <div class="col-md-6">
+            <input type="email" class="email" placeholder="Your email adress" v-bind="email.adress">
+            <textarea
+              class="message"
+              placeholder="Type your message here..."
+              v-bind="email.message"
+            ></textarea>
+            <div class="send">
+              <button class="btn-send" @click="sendMail">Send this message</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -40,24 +53,38 @@ import VueMarkdown from "vue-markdown";
 import Strapi from "strapi-sdk-javascript/build/main";
 const apiUrl = process.env.API_URL || "http://localhost:1337";
 const strapi = new Strapi(apiUrl);
+const email = {};
 
 export default {
   data() {
     return {
-      about: this.$store.getters["about/list"][0]
+      about: this.$store.getters["about/list"][0],
+      email: {
+        adress: "",
+        message: ""
+      }
     };
   },
   methods: {
-    sendMail: function() {
-      const self = this;
+    async sendMail() {
+      try {
+        email.to = this.about.email;
+        email.from = this.email.adress;
+        email.text = this.email.message;
 
-      strapi.plugins["email"].services.email.send({
-        to: self.about.email,
-        from: "robbot@strapi.io",
-        replyTo: "no-reply@strapi.io",
-        subject: "Use strapi email provider successfully",
-        text: "Hello world!"
-      });
+        console.log(strapi, "strapi");
+        console.log(strapi.services, "strapi-services");
+
+        const reponse = await strapi.services.email.send({
+          to: email.to,
+          from: email.from,
+          replyTo: email.from,
+          subject: "ORPHEVS.fr - Vous avez reçu un nouveau message",
+          text: email.text
+        });
+      } catch (err) {
+        console.error(err.message || "An error occurred.");
+      }
     }
   },
   async fetch({ store, params }) {
