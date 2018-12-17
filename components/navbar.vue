8ac48f1d81b5afccccea0cc5428a39f4b0845814
ClementGuerin <clement.guerin@hetic.net>
2018-12-17 15:14:07 +0100

    update about page + add animation

--- components/navbar.vue
+++ components/navbar.vue
@@ -12,17 +12,64 @@
         <span></span>
       </a>
     </div>
+    <div class="menuAnimation"></div>
   </section>
 </template>
 
 <script>
+import lottie from "lottie-web";
+import menuAnimation from "~/assets/animations/menu.json";
+
 export default {
   data() {
-    return {};
+    return {
+      menuAnim: null,
+      menuState: false
+    };
+  },
+  mounted() {
+    const self = this;
+
+    this.menuAnim = lottie.loadAnimation({
+      container: document.querySelector(".menuAnimation"), // the dom element that will contain the animation
+      renderer: "svg",
+      loop: false,
+      autoplay: false,
+      animationData: menuAnimation,
+      rendererSettings: {
+        scaleMode: "noScale",
+        clearCanvas: false,
+        progressiveLoad: false, // Boolean, only svg renderer, loads dom elements when needed. Might speed up initialization for large number of elements.
+        hideOnTransparent: true //Boolean, only svg renderer, hides elements when opacity reaches 0 (defaults to true)
+      }
+    });
+
+    this.menuAnim.setSpeed(2);
+
+    $(".menuAnimation").hide();
+
+    this.menuAnim.addEventListener("enterFrame", function() {
+      $(".menuAnimation").show();
+    });
+
+    this.menuAnim.addEventListener("complete", function() {
+      if (!self.menuState) {
+        $(".menuAnimation").hide();
+      }
+    });
+
+    $(".menuAnimation > svg").attr("viewBox", "");
   },
   methods: {
     toggleMenu: function() {
-      console.log("menu toggled");
+      if (this.menuState) {
+        this.menuAnim.setDirection("-1");
+        this.menuAnim.play();
+      } else {
+        this.menuAnim.setDirection("1");
+        this.menuAnim.play();
+      }
+      this.menuState = !this.menuState;
     }
   }
 };
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -12,16 +12,19 @@
           <vue-markdown class="description">{{ about.description }}</vue-markdown>
           <ul class="social">
             <li>
-              <a href="#">CV</a>
+              <a href="https://orphevs.fr/cv.pdf" target="_blank">CV</a>
             </li>
             <li>
-              <a href="#">Twitter</a>
+              <a href="https://twitter.com/Orphevs" target="_blank">Twitter</a>
             </li>
             <li>
-              <a href="#">Linkedin</a>
+              <a
+                href="https://www.linkedin.com/in/cl%C3%A9ment-gu%C3%A9rin-68a690152/"
+                target="_blank"
+              >Linkedin</a>
             </li>
             <li>
-              <a href="#">Malt</a>
+              <a href="https://www.malt.fr/profile/clementguerin1" target="_blank">Malt</a>
             </li>
           </ul>
         </div>
@@ -29,9 +32,9 @@
     </div>
     <div class="contact">
       <div class="container">
-        <h2 class="title">Contact me</h2>
+        <h2 class="title" v-if="formState">Contact me</h2>
         <div class="form row justify-content-center">
-          <div class="col-md-6">
+          <div class="col-md-6 form-tocomplete" v-if="formState">
             <input
               type="email"
               class="email"
@@ -46,10 +49,24 @@
               v-model="email.message"
             ></textarea>
             <div class="send">
-              <vue-recaptcha :sitekey="sitekey" @verify="onVerify">
+              <vue-recaptcha :sitekey="sitekey" @verify="onVerify" ref="recaptcha">
                 <button class="btn-send">Send this message</button>
               </vue-recaptcha>
             </div>
+          </div>
+          <div class="col-md-6 form-send" v-if="!formState">
+            <div class="check_mark">
+              <div class="sa-icon sa-success animate">
+                <span class="sa-line sa-tip animateSuccessTip"></span>
+                <span class="sa-line sa-long animateSuccessLong"></span>
+                <div class="sa-placeholder"></div>
+                <div class="sa-fix"></div>
+              </div>
+            </div>
+            <h3 class="form-send-title">Your message has been sent !</h3>
+            <p
+              class="form-send-message"
+            >I will answer you as soon as possible, you will receive a confirmation mail</p>
           </div>
         </div>
       </div>
@@ -74,13 +91,27 @@ export default {
       email: {
         adress: "",
         message: ""
-      }
+      },
+      formState: true
     };
   },
   methods: {
     onVerify: function(response) {
       if (response) {
-        this.sendMail();
+        if (this.email.adress !== "" && this.email.message !== "") {
+          if (
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+              this.email.adress
+            )
+          ) {
+            this.sendMail();
+          } else {
+            alert("Email address is not correct.");
+          }
+        } else {
+          alert("Please fill all fields correctly.");
+        }
+        this.resetRecaptcha();
       }
     },
     async sendMail() {
@@ -98,24 +129,34 @@ export default {
             text: emailInfos.body
           }
         });
+
+        this.formState = false;
       } catch (err) {
-        alert(err);
-        console.log(options);
+        console.error(err);
+        this.formState = true;
       }
 
-      // axios
-      //   .post("https://orphevs.fr/mail.php", {
-      //     to: emailInfos.to,
-      //     from: emailInfos.from,
-      //     subject: "ORPHEVS - Nouveau message",
-      //     message: emailInfos.body
-      //   })
-      //   .then(function(response) {
-      //     console.log(response);
-      //   })
-      //   .catch(function(error) {
-      //     alert(error);
-      //   });
+      try {
+        const reponse = await strapi.request("post", "/email", {
+          data: {
+            to: emailInfos.from,
+            from: emailInfos.to,
+            replyTo: emailInfos.to,
+            subject: "ORPHEVS - Your message has been sent!",
+            html: `<b>Hello ${emailInfos.from} !</b>
+            <p>Your message has been sent and I will answer you soon as possible !</p>
+            <br>
+            <p>Your message : <i>${emailInfos.body}</i></p>
+            <br>
+            <span>Sincerely, <a href="https://orphevs.fr" target="_blank">Orphevs</a></span>`
+          }
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    resetRecaptcha() {
+      this.$refs.recaptcha.reset(); // Direct call reset method
     }
   },
   async fetch({ store, params }) {
